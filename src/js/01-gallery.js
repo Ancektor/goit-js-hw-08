2813453b939c83dc27cebdc75b3e7e1b36b81262
Ancektor <ancektor@gmail.com>
2023-09-05 10:48:18 +0300

    123

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,8 @@
-// Add imports above this line
-import { galleryItems } from "./gallery-items";
-// Change code below this line
+// Описан в документации
 import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
 import "simplelightbox/dist/simple-lightbox.min.css";
+// import "simplelightbox/dist/simple-lightbox.min.css";
 const galleryEl = document.querySelector('.gallery');
 
 function createdElementGallery(galleryItems) {
